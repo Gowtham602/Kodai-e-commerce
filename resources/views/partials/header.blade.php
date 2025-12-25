b1ec6cfe459779e1ec5cc3cf74a68ac88d74b415
Gowtham602 <gowthamsundararajan7777@gmail.com>
2025-12-25 10:46:02 +0530

    Login page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,18 +15,21 @@
         <div class="nav-links" id="navbar-menu">
             <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
             <a href="{{ route('products.index') }}" class="{{ request()->routeIs('products.index') ? 'active' : '' }}">Products</a>
-            <a href="{{ route('about') }}#about">About</a>
-            <a href="{{ route('home') }}#contact">Contact</a>
+            <a href="{{ route('about') }}">About</a>
+            <a href="{{ route('home') }}">Contact</a>
 
             @guest
-            <a href="{{ route('login') }}" class="btn login">Login</a>
-            <a href="{{ route('register') }}" class="btn register">Register</a>
+            {{-- <a href="{{ route('login') }}" class="btn login">Login</a> --}}
+            {{-- <a href="{{ route('register') }}" class="btn register">Register</a> --}}
+            <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a>
+<a href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Register</a>
+
             @else
             <!-- User Dropdown -->
             <div class="dropdown">
-                <button class="dropbtn">{{ Auth::user()->name }} &#x25BC;</button>
+                <button class="dropbtn" id="userDropdownBtn">{{ Auth::user()->name }} &#x25BC;</button>
                 <div class="dropdown-content">
-                    <a href="{{ route('profile.edit') }}">Profile</a>
+                    <a href="{{ route('profile.edit') }}" class="text-black p-3">Profile</a>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit" class="logout-btn">Logout</button>
@@ -37,8 +40,8 @@
 
             <!-- Cart Icon -->
           <a href="{{ route('cart.index') }}" class="cart">
-            <i class="fas fa-shopping-cart"></i>
-            <span id="cart-count">0</span>
+            <i class="fas fa-shopping-cart text-bold"></i>
+            <span id="cart-count">Order</span>
         </a>
 
         </div>
